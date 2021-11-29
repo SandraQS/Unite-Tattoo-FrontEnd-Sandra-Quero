573e27637540d0448461eafe06e4669459e50a03
SandraQS <sanquesan@gmail.com>
2021-11-29 20:21:27 +0100

    Change Home for view the new component

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,30 +1,7 @@
-import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { Button, Text, TouchableOpacity } from "react-native";
-import Prueba from "../../components/Prueba/Prueba";
-import RoutesEnum from "../../navigation/routes";
-import { LoginScreenNavigationProp } from "../../types/navigation.types";
+
+import Register from "../Register/Register";
 
 export const Home = () => {
-  const navigation = useNavigation<LoginScreenNavigationProp>();
-
-  return (
-    <>
-      <Prueba />
-      <Button
-        title="Vamos a Prueba2"
-        onPress={() => {
-          navigation.navigate(RoutesEnum.login);
-        }}
-      />
-      <TouchableOpacity
-        onLongPress={() => {
-          navigation.navigate(RoutesEnum.login);
-        }}
-        activeOpacity={0.6}
-      >
-        <Text>BUTTON</Text>
-      </TouchableOpacity>
-    </>
-  );
+  return <Register />;
 };
