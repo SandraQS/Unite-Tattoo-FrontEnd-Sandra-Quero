ee482960f98488103519a6a234954d84a3e67e4f
SandraQS <sanquesan@gmail.com>
2021-11-29 13:29:17 +0100

    Add ts at MainNavigator and Home

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -3,11 +3,11 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RoutesEnum from "./routes";
 import { Home } from "../screens/Home/Home";
 import { Login } from "../screens/Login/Login";
+import { RootStackParamList } from "../types/types";
 
 //mainnavigator
 export const MainNavigator = () => {
-  const Stack = createNativeStackNavigator();
-
+  const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <Stack.Navigator initialRouteName={RoutesEnum.home}>
       <Stack.Screen name={RoutesEnum.home} component={Home} />
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,16 +2,19 @@ import { useNavigation } from "@react-navigation/core";
 import React from "react";
 import { Button } from "react-native";
 import Prueba from "../../components/Prueba/Prueba";
+import RoutesEnum from "../../navigation/routes";
+import type { LoginScreenNavigationProp } from "../../types/types";
 
 export const Home = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<LoginScreenNavigationProp>();
+
   return (
     <>
       <Prueba />
       <Button
         title="Vamos a Prueba2"
         onPress={() => {
-          navigation.navigate("Login");
+          navigation.navigate(RoutesEnum.login);
         }}
       />
     </>
