8c7b931f70fa85456034cddeac0eb98a6c668ec0
SandraQS <sanquesan@gmail.com>
2021-11-29 13:54:35 +0100

    Save files for actualize lf

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainNavigator } from "./navigation/MainNavigator";
-import {
-  Button,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  View,
-} from "react-native";
 
 const App = () => {
   return (
