db79990002716a2b66d8bbee95ef0ca88f7783a0
SandraQS <sanquesan@gmail.com>
2021-11-29 13:26:57 +0100

    Add rules and configuration at editconfig and eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react", "@typescript-eslint", "prettier", "jest"],
   extends: [
-    "@react-native-community",
+    // "@react-native-community",
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
@@ -19,29 +19,20 @@ module.exports = {
   rules: {
     "no-multi-spaces": ["error"],
     "no-multiple-empty-lines": ["error", { max: 1 }],
-    "object-curly-spacing": ["error", "always"],
+    "object-curly-spacing": [
+      "error",
+      "always",
+      { arraysInObjects: true, objectsInObjects: false },
+    ],
     "no-use-before-define": ["off"],
     "no-console": ["error", { allow: ["warn", "error"] }],
-    "@typescript-eslint/naming-convention": [
-      "warn",
-      {
-        selector: "interface",
-        format: ["PascalCase"],
-        custom: {
-          regex: "^I[A-Z]",
-          match: true,
-        },
-      },
-    ],
     "@typescript-eslint/no-use-before-define": ["off"],
     "@typescript-eslint/no-var-requires": ["warn"],
     "@typescript-eslint/no-empty-function": ["warn"],
-    "@typescript-eslint/camelcase": ["off"],
     "@typescript-eslint/no-empty-interface": ["warn"],
     "react-native/no-inline-styles": ["error"],
     "react-native/no-raw-text": ["warn"],
     "react-native/split-platform-components": ["warn"],
-    "react-native/sort-styles": "off",
     "react-native/no-color-literals": ["warn"],
     "react-native/no-unused-styles": ["warn"],
     "comma-dangle": ["error", "always-multiline"],
