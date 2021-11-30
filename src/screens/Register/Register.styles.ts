7b9ace198440af557096d993358195be644589f1
SandraQS <sanquesan@gmail.com>
2021-11-30 09:20:06 +0100

    Add styles at form register tattoo artist

---
+++ src/screens/Register/Register.styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+import { uniteTattoo } from "../../styles/uniteTatto.styles";
+
+const { color, fontSize } = uniteTattoo;
+export default StyleSheet.create({
+  input: {
+    borderBottomColor: color.black,
+    borderBottomWidth: 1,
+    padding: 20,
+  },
+
+  mainContainer: {
+    backgroundColor: color.mediumBrown,
+    padding: 20,
+  },
+
+  personalDataContainer: {
+    marginTop: 30,
+  },
+  text: {
+    fontSize: fontSize.text,
+    textAlign: "center",
+  },
+  tittleH2: {
+    fontSize: fontSize.tittleH2,
+    textAlign: "center",
+  },
+
+  welcomeContainer: {
+    alignContent: "center",
+    backgroundColor: color.lightBrown,
+    flex: 1,
+    padding: 30,
+  },
+});
