284c059bb67748a019a560e38b4a5744c78df420
SandraQS <sanquesan@gmail.com>
2021-12-01 18:02:04 +0100

    Add works at index

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import tattooArtistReducer from "./tattooArtistReducer";
+import worksReducer from "./worksREducer";
 
 const rootReducer = combineReducers({
   tattooArtist: tattooArtistReducer,
+  work: worksReducer,
 });
 
 export default rootReducer;
