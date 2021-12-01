d230ca69d572720e9e0a548e64f819cf4239a118
SandraQS <sanquesan@gmail.com>
2021-12-01 21:44:37 +0100

    Deletet unused vars

--- src/screens/AllWorks/AllWorks.tsx
+++ src/screens/AllWorks/AllWorks.tsx
@@ -1,6 +1,4 @@
-import { useNavigation } from "@react-navigation/core";
-import React, { useEffect, useState } from "react";
-import { colors } from "../../styles/uniteTatto.styles";
+import React, { useEffect } from "react";
 
 import {
   SafeAreaView,
@@ -8,13 +6,11 @@ import {
   StatusBar,
   Text,
   View,
-  Image,
-  FlatList,
+  // FlatList,
 } from "react-native";
 import Title from "../../components/Title/Title";
 import TalentCard from "../../components/TalentCard/TalentCard";
 import { useWorks } from "../../hooks/useWorks";
-import { TalentCardProps } from "../../types/components.types";
 import { IWork } from "../../types/interfacesComponent";
 
 const AllWorks = () => {
