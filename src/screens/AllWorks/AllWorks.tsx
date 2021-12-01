95c6786d405f9e5bf9e15ff2d202e27748178fc9
SandraQS <sanquesan@gmail.com>
2021-12-01 17:24:21 +0100

    Add component title and talentCart at Allworks screen

--- src/screens/AllWorks/AllWorks.tsx
+++ src/screens/AllWorks/AllWorks.tsx
@@ -10,6 +10,8 @@ import {
   View,
   Image,
 } from "react-native";
+import Title from "../../components/Title/Title";
+import TalentCard from "../../components/TalentCard/TalentCard";
 
 const AllWorks = () => {
   return (
@@ -17,7 +19,14 @@ const AllWorks = () => {
       <StatusBar />
       <ScrollView>
         <View>
-          <Image source={require("../../assets/Uttoo-logo.svg")} />
+          <Title text="TALENTOS " textBold="DESTACADOS" />
+
+          <TalentCard
+            imageWork="imagen"
+            tattooArtist="Bruno"
+            styleWork="fine line"
+            functionOnPress={() => {}}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,20 +1,8 @@
 import React from "react";
-import TalentCard from "../../components/TalentCard/TalentCard";
-import Title from "../../components/Title/Title";
+import AllWorks from "../AllWorks/AllWorks";
 
 import UniteTattoo from "../UniteTattoo/UniteTattoo";
 
 export const Home = () => {
-  return (
-    <>
-      <Title text="HOLA" textBold="QUETAL" />
-
-      <TalentCard
-        imageWork="imagen"
-        tattooArtist="Bruno"
-        styleWork="fine line"
-        functionOnPress={() => {}}
-      />
-    </>
-  );
+  return <AllWorks />;
 };
