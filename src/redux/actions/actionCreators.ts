c484d9ba1cc60279a756a31a1cfbe03fb465228a
SandraQS <sanquesan@gmail.com>
2021-12-01 17:40:49 +0100

    Add actiontypes and actioncreatos for load all works

--- src/redux/actions/actionCreators.ts
+++ src/redux/actions/actionCreators.ts
@@ -4,3 +4,8 @@ export const registerTattooArtistAction = (tattooArtist: object) => ({
   type: actionTypes.createTattooArtist,
   tattooArtist,
 });
+
+export const loadAllWorksAction = (works: Array<object>) => ({
+  type: actionTypes.loadAllWorks,
+  works,
+});
--- src/redux/actions/actionTypes.ts
+++ src/redux/actions/actionTypes.ts
@@ -1,5 +1,7 @@
 const actionTypes = {
   createTattooArtist: "CREATE_TATTOOARTIST",
+
+  loadAllWorks: "LOAD_ALL_WORKS",
 };
 
 export default actionTypes;
