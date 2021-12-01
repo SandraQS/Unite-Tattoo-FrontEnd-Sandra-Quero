80dea6cd6585394f26d8d25ecc4d4578c9fa1366
SandraQS <sanquesan@gmail.com>
2021-12-01 15:30:22 +0100

    Add types for TalentCard

--- src/types/components.types.ts
+++ src/types/components.types.ts
@@ -2,3 +2,10 @@ export type GeneralButtonProps = {
   textButton: string;
   functionOnPress: () => void;
 };
+
+export type TalentCard = {
+  imageWork: string;
+  titleWork: string;
+  subtitleWork: string;
+  functionOnPress?: () => void;
+};
