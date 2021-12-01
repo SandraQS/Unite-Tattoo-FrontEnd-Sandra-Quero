378eefc2d0c7c585a8403cabc98e4af043d9d161
SandraQS <sanquesan@gmail.com>
2021-12-01 16:35:25 +0100

    Add styoles at TAlentCardComponent

--- src/components/TalentCard/TalentCard.styles.ts
+++ src/components/TalentCard/TalentCard.styles.ts
@@ -6,5 +6,38 @@ import {
 } from "../../styles/uniteTatto.styles";
 
 export default StyleSheet.create({
-  button: {},
+  imageContainer: {
+    alignContent: "center",
+    borderRadius: 20,
+    flex: 3,
+    height: 200,
+    justifyContent: "center",
+    padding: 10,
+  },
+  imageWork: {
+    borderRadius: 20,
+    flex: 5,
+  },
+  mainContainerCard: {
+    height: 300,
+    width: 150,
+  },
+  styleSubtitle: {
+    color: colors.mediumBrown,
+    fontSize: 15,
+  },
+  talentCard: {
+    backgroundColor: colors.lightBrown,
+    borderRadius: 20,
+    flex: 1,
+    flexDirection: "column",
+  },
+  tattooArtistTitle: {
+    color: colors.darkBrown,
+    fontSize: 20,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
 });
--- src/components/TalentCard/TalentCard.tsx
+++ src/components/TalentCard/TalentCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { ITalentCard } from "../../types/components.types";
 
+import style from "./TalentCard.styles";
+
 const TalentCard = ({
   imageWork,
   tattooArtist,
@@ -9,17 +11,20 @@ const TalentCard = ({
   functionOnPress,
 }: ITalentCard) => {
   return (
-    <View>
-      <View>
-        <Image
-          source={{
-            uri: "https://logiabarcelona.com/wp-content/uploads/photo-gallery/Bruno%20Almeida/tatuaje_brazo_flores_plantas_logia_barcelona_bruno_almeida.jpg",
-          }}
-        />
+    <View style={style.mainContainerCard}>
+      <View style={style.talentCard}>
+        <View style={style.imageContainer}>
+          <Image
+            source={{
+              uri: "https://logiabarcelona.com/wp-content/uploads/photo-gallery/Bruno%20Almeida/tatuaje_brazo_flores_plantas_logia_barcelona_bruno_almeida.jpg",
+            }}
+            style={style.imageWork}
+          />
 
-        <View>
-          <Text>{tattooArtist}</Text>
-          <Text>{styleWork}</Text>
+          <View style={style.textContainer}>
+            <Text style={style.tattooArtistTitle}>{tattooArtist}</Text>
+            <Text style={style.styleSubtitle}>{styleWork}</Text>
+          </View>
         </View>
       </View>
     </View>
