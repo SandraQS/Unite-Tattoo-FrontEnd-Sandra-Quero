992ab5727c24165c42e90d54953d565d05f87ec5
SandraQS <sanquesan@gmail.com>
2021-12-01 17:41:21 +0100

    Create worksReducer

---
+++ src/redux/reducers/worksReducer.ts
@@ -0,0 +1,19 @@
+import { ItattooWorksActions } from "../../types/interfacesFlux";
+import actionTypes from "../actions/actionTypes";
+
+const worksReducer = (works = [], action: ItattooWorksActions) => {
+  let newWorks;
+
+  switch (action.type) {
+    case actionTypes.loadAllWorks:
+      newWorks = [...action.works];
+      break;
+
+    default:
+      newWorks = works;
+  }
+
+  return newWorks;
+};
+
+export default worksReducer;
