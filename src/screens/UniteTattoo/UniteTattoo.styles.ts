48bff9acfc778746b3e2471c185bc2f2b68a3ec4
SandraQS <sanquesan@gmail.com>
2021-12-02 00:44:33 +0100

    Add styles at screen

--- src/screens/UniteTattoo/UniteTattoo.styles.ts
+++ src/screens/UniteTattoo/UniteTattoo.styles.ts
@@ -1,11 +1,48 @@
 import { StyleSheet } from "react-native";
-import { fontSize } from "../../styles/uniteTatto.styles";
+import { fontSize, colors } from "../../styles/uniteTatto.styles";
 
 export default StyleSheet.create({
+  buttonInvited: {
+    flex: 1,
+  },
   buttonUniteTattoo: {
     padding: 20,
   },
+  containerInvited: {
+    flex: 1,
+  },
+  containerUsers: {
+    flex: 4,
+    justifyContent: "center",
+  },
+  imageBackground: {
+    alignSelf: "stretch",
+    flex: 1,
+    justifyContent: "center",
+  },
+  invited: {
+    fontSize: 30,
+    textAlign: "center",
+  },
+  logo: {
+    margin: 20,
+  },
+  logoContainter: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mainContainer: {
+    backgroundColor: colors.mediumBrown,
+    flex: 1,
+    height: 700,
+    padding: 20,
+  },
+  separator: {
+    borderBottomWidth: 2,
+    borderColor: colors.black,
+  },
   text: {
-    fontSize: fontSize.tittleH2,
+    fontSize: 40,
+    textAlign: "center",
   },
 });
--- src/screens/UniteTattoo/UniteTattoo.tsx
+++ src/screens/UniteTattoo/UniteTattoo.tsx
@@ -7,15 +7,16 @@ import {
   StatusBar,
   Text,
   View,
-  Image,
   TouchableOpacity,
   Button,
+  Image,
+  ImageBackground,
 } from "react-native";
 
 import styles from "./UniteTattoo.styles";
-import { mainContainer } from "../../styles/containers.styles";
 import RoutesEnum from "../../navigation/routes";
 import { UniteTattooScreenNavigationProp } from "../../types/navigation.types";
+import AutoHeightImage from "react-native-auto-height-image";
 
 const UniteTattoo = () => {
   const navigation = useNavigation<UniteTattooScreenNavigationProp>();
@@ -24,43 +25,50 @@ const UniteTattoo = () => {
     <SafeAreaView>
       <StatusBar />
       <ScrollView>
-        <View style={mainContainer}>
-          <View>
-            <Button
-              title="Vamos a HOME"
-              onPress={() => {
-                navigation.navigate(RoutesEnum.home);
-              }}
+        <View style={styles.mainContainer}>
+          <View style={styles.logoContainter}>
+            <AutoHeightImage
+              width={250}
+              source={require("../../assets/uniteTattoo_logo.png")}
+              style={styles.logo}
             />
           </View>
-
-          <TouchableOpacity
-            style={styles.buttonUniteTattoo}
-            activeOpacity={0.7}
-            onPress={() => {
-              navigation.navigate(RoutesEnum.home);
-            }}
+          <ImageBackground
+            source={require("../../assets/icono-rombo-fondo-01.png")}
+            resizeMode="contain"
+            style={styles.imageBackground}
           >
-            <Text style={styles.text}> Tatuador </Text>
-          </TouchableOpacity>
+            <View style={styles.containerUsers}>
+              <TouchableOpacity
+                style={[styles.buttonUniteTattoo, styles.separator]}
+                activeOpacity={0.7}
+                onPress={() => {
+                  navigation.navigate(RoutesEnum.home);
+                }}
+              >
+                <Text style={styles.text}> Tatuador </Text>
+              </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.buttonUniteTattoo}
-            activeOpacity={0.7}
-            onPress={() => {
-              navigation.navigate(RoutesEnum.login);
-            }}
-          >
-            <Text style={styles.text}> Cliente </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.buttonUniteTattoo}
-            activeOpacity={0.7}
-            onPress={() => navigation.navigate(RoutesEnum.home)}
-          >
-            <Text style={styles.text}> Entrar como invitado </Text>
-          </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.buttonUniteTattoo}
+                activeOpacity={0.7}
+                onPress={() => {
+                  navigation.navigate(RoutesEnum.login);
+                }}
+              >
+                <Text style={styles.text}> Cliente </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.containerInvited}>
+              <TouchableOpacity
+                style={[styles.buttonUniteTattoo, styles.buttonInvited]}
+                activeOpacity={0.7}
+                onPress={() => navigation.navigate(RoutesEnum.home)}
+              >
+                <Text style={styles.invited}> Entrar como invitado </Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
         </View>
       </ScrollView>
     </SafeAreaView>
