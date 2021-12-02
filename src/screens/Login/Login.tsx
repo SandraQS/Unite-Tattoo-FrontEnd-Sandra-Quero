a780fcd91cda54fe088d26d10cd90876269ef234
SandraQS <sanquesan@gmail.com>
2021-12-02 14:08:38 +0100

    Add styles and form

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,19 +1,17 @@
 import { useNavigation } from "@react-navigation/core";
 import { LoginScreenNavigationProp } from "../../types/navigation.types";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
-  Text,
   View,
-  Image,
   TextInput,
 } from "react-native";
 
 import styles from "./Login.styles";
+import { generalStyles, colors } from "../../styles/uniteTatto.styles";
 
 import RoutesEnum from "../../navigation/routes";
 import GeneralButton from "../../components/GeneralButton/GeneralButton";
@@ -21,52 +19,72 @@ import AutoHeightImage from "react-native-auto-height-image";
 
 export const Login = () => {
   const navigation = useNavigation<LoginScreenNavigationProp>();
+  const initialUserData = { email: "", password: "" };
+  const [userData, setUserData] = useState(initialUserData);
 
   return (
-    <>
-      <Button
-        title="Registrarme"
-        onPress={() => {
-          navigation.navigate(RoutesEnum.register);
-        }}
-      />
-      <SafeAreaView>
-        <StatusBar />
-        <ScrollView>
-          <View style={styles.mainContainer}>
-            <View style={styles.logoContainter}>
-              <AutoHeightImage
-                width={150}
-                source={require("../../assets/utoo-logo.png")}
-                style={styles.logo}
-              />
-            </View>
+    <SafeAreaView style={generalStyles.screenMediumBrown}>
+      <ScrollView>
+        <View style={styles.mainContainer}>
+          <View style={styles.logoContainter}>
+            <AutoHeightImage
+              width={150}
+              source={require("../../assets/utoo-logo.png")}
+              style={styles.logo}
+            />
+          </View>
+
+          <View style={styles.forms}>
             <View>
-              <Text style={styles.tittleH2}>Login</Text>
-            </View>
-
-            <View style={styles.forms}>
               <View>
-                <View>
-                  <TextInput
-                    style={styles.input}
-                    value={"hola"}
-                    placeholder="Nombre *"
-                    onChange={() => {}}
-                    testID="name"
-                    maxLength={20}
-                  />
-                </View>
-
-                <GeneralButton
-                  textButton="REGISTRARME COMO TATUADOR"
-                  functionOnPress={() => {}}
+                <TextInput
+                  style={styles.input}
+                  value={userData.email}
+                  keyboardType="email-address"
+                  placeholder="Email *"
+                  onChange={() => {}}
+                  accessibilityLabel="email"
+                  maxLength={20}
                 />
               </View>
+
+              <View>
+                <TextInput
+                  style={styles.input}
+                  value={userData.password}
+                  onChange={() => {}}
+                  // onFocus={}
+                  placeholder="Contraseña *"
+                  accessibilityLabel="password"
+                  secureTextEntry={true}
+                  maxLength={20}
+                />
+              </View>
+              <View>
+                <TextInput
+                  style={styles.input}
+                  value={userData.password}
+                  onChange={() => {}}
+                  // onFocus={}
+                  placeholder="Repetir la contraseña *"
+                  secureTextEntry={true}
+                  accessibilityLabel="repeatPassword"
+                  maxLength={20}
+                />
+              </View>
+
+              <GeneralButton textButton="LOGIN" functionOnPress={() => {}} />
+              <Button
+                title="Registrarme"
+                onPress={() => {
+                  navigation.navigate(RoutesEnum.register);
+                }}
+                color={colors.darkBrown}
+              />
             </View>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
