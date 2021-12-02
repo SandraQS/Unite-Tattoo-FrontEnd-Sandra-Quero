b469008f79da25fb7b25a9148a9490285e1319ad
SandraQS <sanquesan@gmail.com>
2021-12-02 23:59:48 +0100

    Call tokenStorage function(fail)

--- src/redux/thunks/userTattooArtistThunk.ts
+++ src/redux/thunks/userTattooArtistThunk.ts
@@ -2,18 +2,30 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { tattooArtistLoginAction } from "../actions/actionCreators";
 
+import AsyncStorage from "@react-native-community/async-storage";
+import { tokenStorage } from "../../storage/asyncStorage";
+
 export const tattooArtistLoginTunk =
-  (userTattooArtist: any) => async (dispatch: any) => {
-    const response = await axios.post(
-      "https://proyecto-final-sandra-back.herokuapp.com/uniteTattoo/tattooArtist/login",
-      userTattooArtist
-    );
+  (userTattooArtist: object) => async (dispatch: any) => {
+    try {
+      // console.log("HOLA???");
+      const response = await axios.post(
+        "https://proyecto-final-sandra-back.herokuapp.com/uniteTattoo/tattooArtist/login",
+        userTattooArtist
+      );
+      // console.log("HOLA???", response);
 
-    if (response.status === 200) {
-      const token = response.data.token;
-      const userRegistered: any = jwtDecode(token);
-      dispatch(tattooArtistLoginAction(userRegistered));
+      if (response.status === 200) {
+        const token = response.data.token;
+        const userRegistered: any = jwtDecode(token);
 
-      localStorage.setItem("userTattooArtis", JSON.stringify({ token: token }));
+        dispatch(tattooArtistLoginAction(userRegistered));
+
+        const resultado = tokenStorage("userRegistered", token);
+
+        // console.log("ESTOQUEESSS", resultado);
+      }
+    } catch (error: any) {
+      return error.message;
     }
   };
