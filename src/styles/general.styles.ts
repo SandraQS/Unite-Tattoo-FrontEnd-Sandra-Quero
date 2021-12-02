3cd42de779068714338221e1d9e987ccc898d44b
SandraQS <sanquesan@gmail.com>
2021-12-02 10:04:00 +0100

    Add styles general for screen and aply at files

---
+++ src/styles/general.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  mainContainerGeneral: {
+    padding: 20,
+  },
+
+  screen: {
+    flex: 1,
+  },
+});
--- src/styles/uniteTatto.styles.ts
+++ src/styles/uniteTatto.styles.ts
@@ -1,5 +1,6 @@
 import { colors } from "./colors.styles";
 import { fontSize } from "./fontSize.styles";
 import { mainContainer } from "./containers.styles";
+import generalStyles from "./general.styles";
 
-export { colors, fontSize, mainContainer };
+export { colors, fontSize, mainContainer, generalStyles };
