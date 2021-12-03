d48c4abf156b45c99929f8fd83615e7822b1c5d8
SandraQS <sanquesan@gmail.com>
2021-12-03 10:18:05 +0100

    Add await for call tokenStorage()

--- src/redux/thunks/userTattooArtistThunk.ts
+++ src/redux/thunks/userTattooArtistThunk.ts
@@ -2,18 +2,15 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { tattooArtistLoginAction } from "../actions/actionCreators";
 
-import AsyncStorage from "@react-native-community/async-storage";
 import { tokenStorage } from "../../storage/asyncStorage";
 
 export const tattooArtistLoginTunk =
   (userTattooArtist: object) => async (dispatch: any) => {
     try {
-      // console.log("HOLA???");
       const response = await axios.post(
         "https://proyecto-final-sandra-back.herokuapp.com/uniteTattoo/tattooArtist/login",
         userTattooArtist
       );
-      // console.log("HOLA???", response);
 
       if (response.status === 200) {
         const token = response.data.token;
@@ -21,9 +18,9 @@ export const tattooArtistLoginTunk =
 
         dispatch(tattooArtistLoginAction(userRegistered));
 
-        const resultado = tokenStorage("userRegistered", token);
+        const localStorage = await tokenStorage("userRegistered", token);
 
-        // console.log("ESTOQUEESSS", resultado);
+        // console.log(localStorage);
       }
     } catch (error: any) {
       return error.message;
