018c09d0d9766836996f0fb0ed2dd185e4a403af
SandraQS <sanquesan@gmail.com>
2021-12-03 01:13:01 +0100

    Add snapshots

--- src/components/NavHeader/NavHeader.test.tsx
+++ src/components/NavHeader/NavHeader.test.tsx
@@ -4,9 +4,10 @@ import NavHeader from "./NavHeader";
 
 describe("Given a NavHeader component", () => {
   describe("When it is rendered", () => {
-    test("Then should match the snapshot", () => {
+    test("Then it should show match the last snapshot", () => {
       const nameUser = "Sandra";
       const screen = render(<NavHeader nameUser={nameUser} />);
+
       expect(screen).toMatchSnapshot();
     });
   });
