12f1ba4b6216047f7d9907a97756da6ea408846e
SandraQS <sanquesan@gmail.com>
2021-12-03 18:53:40 +0100

    Add props for onPress at CreateButton

--- src/components/CreateButton/CreateButton.tsx
+++ src/components/CreateButton/CreateButton.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { TouchableOpacity, View } from "react-native";
 import AutoHeightImage from "react-native-auto-height-image";
+import { CreateButtonProps } from "../../types/components.types";
 
 import style from "./CreateButton.styles";
 
-const CreateButton = () => {
+const CreateButton = ({ functionCreate }: CreateButtonProps) => {
   return (
-    <TouchableOpacity onPress={() => {}} activeOpacity={0.6}>
+    <TouchableOpacity onPress={functionCreate} activeOpacity={0.6}>
       <View style={style.card}>
         <View style={style.imageContainer}>
           <AutoHeightImage
