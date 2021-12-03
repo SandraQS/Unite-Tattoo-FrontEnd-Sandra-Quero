992b9a7094350fc74cc497538c9f139600e2f00d
SandraQS <sanquesan@gmail.com>
2021-12-04 00:32:06 +0100

    Create general styles for forms and delete, for no repeat code

--- src/styles/uniteTatto.styles.ts
+++ src/styles/uniteTatto.styles.ts
@@ -1,6 +1,7 @@
 import { colors } from "./colors.styles";
 import { fontSize } from "./fontSize.styles";
 import { mainContainer } from "./containers.styles";
+import formsStyles from "./forms.styles"
 import generalStyles from "./general.styles";
 
-export { colors, fontSize, mainContainer, generalStyles };
+export { colors, fontSize, mainContainer, generalStyles, formsStyles };
