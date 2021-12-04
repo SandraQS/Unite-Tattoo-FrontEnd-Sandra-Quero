7a8e1b4829165c79a3bc3b50f39f78e51d6762af
SandraQS <sanquesan@gmail.com>
2021-12-04 10:05:05 +0100

    Modified import styles

--- src/styles/uniteTatto.styles.ts
+++ src/styles/uniteTatto.styles.ts
@@ -1,7 +1,6 @@
 import { colors } from "./colors.styles";
 import { fontSize } from "./fontSize.styles";
 import { mainContainer } from "./containers.styles";
-import formsStyles from "./forms.styles"
 import generalStyles from "./general.styles";
 
-export { colors, fontSize, mainContainer, generalStyles, formsStyles };
+export { colors, fontSize, mainContainer, generalStyles };
