184d3efb4c3b2ef15371f6ed8b7ed0b73a70a02a
SandraQS <sanquesan@gmail.com>
2021-12-04 10:08:49 +0100

    Modified expresion to useSelector

--- src/hooks/useUserTattooArtist.ts
+++ src/hooks/useUserTattooArtist.ts
@@ -2,11 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { tattooArtistLoginTunk } from "../redux/thunks/userTattooArtistThunk";
 
 export const useUserTattooArtist = () => {
-  const stateUserTattooArtist: any = useSelector(
-    ({ store: stateUserTattooArtist }: any) => ({
-      store: stateUserTattooArtist,
-    })
-  );
+  const { stateUserTattooArtist }: any = useSelector((store) => store);
   const dispatch = useDispatch();
 
   const tattooArtistLogin = (userTattooArtist: any) => {
