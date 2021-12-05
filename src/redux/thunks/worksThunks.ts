62ad77ae8a98de237dad0b829ca998a125d92f95
SandraQS <sanquesan@gmail.com>
2021-12-05 19:25:16 +0100

    Create thunk for loadWorksCollection

--- src/redux/thunks/worksThunks.ts
+++ src/redux/thunks/worksThunks.ts
@@ -1,19 +1,48 @@
 import axios from "axios";
-import { loadAllWorksAction } from "../actions/actionCreators";
+import {
+  loadAllWorksAction,
+  loadWorksCollectionAction,
+} from "../actions/actionCreators";
 
 import { REACT_APP_URL_API_UNITETATTOO } from "@env";
+import { ICollection } from "../../types/interfacesComponent";
+import { getDataObject } from "../../storage/asyncStorage";
 
 export const loadAllWorksThunk = () => async (dispatch: any) => {
-  try{
+  try {
     const response = await axios.get(
-    `${REACT_APP_URL_API_UNITETATTOO}/client/home`
-  );
+      `${REACT_APP_URL_API_UNITETATTOO}/client/home`
+    );
 
-  if (response.status === 200) {
-    dispatch(loadAllWorksAction(response.data.works));
+    if (response.status === 200) {
+      dispatch(loadAllWorksAction(response.data.works));
+    }
+  } catch (error) {
+    return error;
   }
-  }catch(error){
-    return error
-  }
-  
 };
+
+export const loadWorksCollectionThunk =
+  (collection:ICollection) => async (dispatch: any) => {
+    try {
+      const { token } = await getDataObject("userTattooArtist");
+      if (token) {
+        const response = await axios.get(
+          `${REACT_APP_URL_API_UNITETATTOO}tattooArtist/works/${collection.id}`,
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+
+        console.log("RESPONSE", response);
+        console.log("DATA", response.data)
+        if (response.status === 200) {
+          dispatch(loadWorksCollectionAction(response.data));
+        }
+      }
+    } catch (error) {
+      return error;
+    }
+  };
