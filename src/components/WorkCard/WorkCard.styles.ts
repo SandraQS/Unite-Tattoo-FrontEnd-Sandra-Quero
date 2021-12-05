42575f17ac26f5ec8e678d1b8d3cf96528815b7c
SandraQS <sanquesan@gmail.com>
2021-12-05 20:42:24 +0100

    Add styles at component

--- src/components/WorkCard/WorkCard.styles.ts
+++ src/components/WorkCard/WorkCard.styles.ts
@@ -1,16 +1,43 @@
 import { StyleSheet } from "react-native";
-import { fontSize } from "../../styles/uniteTatto.styles";
+import { fontSize, colors } from "../../styles/uniteTatto.styles";
 
 export default StyleSheet.create({
-  buttonsEditDelete: {},
-  imageContainer: {},
+  buttonsEditDelete: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+  },
+  imageContainer: {
+    position: "relative",
+  },
   imageWork: {
     flex: 1,
-    height: 200,
+    height: 350,
   },
-  styleWork: {},
-  textContainer: {},
-  textWork: {},
-  titleWork: {},
-  workContainer: {},
+  styleWork: {
+    color: colors.darkGrey,
+    fontSize: fontSize.tittleH3,
+    fontWeight: "800",
+    textTransform: "uppercase",
+  },
+  textContainer: {
+    padding: 15,
+  },
+  textWork: {
+    color: colors.darkGrey,
+    fontSize: fontSize.text,
+    fontWeight: "200",
+  },
+  titleWork: {
+    color: colors.black,
+    fontSize: fontSize.tittleH2,
+    fontWeight: "900",
+    textTransform: "uppercase",
+  },
+  workContainer: {
+    borderColor: colors.mediumBrown,
+    borderWidth: 1,
+    margin: 10,
+    position: "relative",
+  },
 });
--- src/styles/fontSize.styles.ts
+++ src/styles/fontSize.styles.ts
@@ -1,7 +1,7 @@
 export const fontSize = {
   titleComponent: 20,
   tittleH2: 25,
-  tittleH3: 20,
+  tittleH3: 16,
   title: 18,
   text: 18,
   textButton: 18,
