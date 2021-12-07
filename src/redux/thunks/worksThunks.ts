f6f4bfb97980a92c1c73cb4bc961f8b86ab3d592
SandraQS <sanquesan@gmail.com>
2021-12-07 16:16:32 +0100

    Delete console.log

--- src/redux/thunks/worksThunks.ts
+++ src/redux/thunks/worksThunks.ts
@@ -49,7 +49,6 @@ export const createWorkThunk =
   (work: Omit<IWork, "id">, idCollection: string) => async (dispatch: any) => {
     try {
       const { token } = await getDataObject("userTattooArtist");
-
       const response = await axios.post(
         `${REACT_APP_URL_API_UNITETATTOO}/tattooArtist/work/create/${idCollection}`,
         work,
