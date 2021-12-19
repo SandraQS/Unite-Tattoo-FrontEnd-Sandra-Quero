8586232e2a5278c842ef140a669ec02082def4fb
SandraQS <sanquesan@gmail.com>
2021-12-19 20:22:00 +0100

    Create styles for PorfileTattooArtist screen

---
+++ src/screens/PorfileTattooArtist/PorfileTattooArtist.styles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../styles/uniteTatto.styles";
+
+export default StyleSheet.create({
+  imageAmbient: {
+    height: 250,
+    opacity: 0.5,
+    width: "100%",
+  },
+
+  imageBackground: {
+    height: 150,
+    position: "relative",
+    top: -90,
+    width: 150,
+  },
+  imagePorfile: {
+    height: 120,
+    left: -20,
+    position: "absolute",
+    top: -20,
+    transform: [{ rotateY: "0deg" }, { rotateZ: "-45deg" }],
+    width: 120,
+  },
+  imagePorfileContainer: {
+    bottom: 19,
+    height: 82,
+    left: 34,
+    overflow: "hidden",
+    position: "absolute",
+    transform: [{ rotateY: "25deg" }, { rotateZ: "45deg" }],
+    width: 82,
+  },
+});
